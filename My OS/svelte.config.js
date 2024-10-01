790b9fb764eca786b5e2ea8af6dbc2573f953af4
PD <89239979+PDgaming@users.noreply.github.com>
2024-10-01 20:06:49 +0530

    temporary fix to a bug

--- My OS/svelte.config.js
+++ My OS/svelte.config.js
@@ -9,9 +9,20 @@ const config = {
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				// ignore missing favicon
+				if (path === '/favicon.png') {
+					return;
+				}
+ 
+				// otherwise fail the build
+				throw new Error(message);
+			}
+		}
 	}
 };
 
